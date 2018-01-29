66617e6b0657934fd12a621c497a5d287fa36c3f
Marcelo Rusconi <mgrusconi@gmail.com>
2018-01-29 16:26:58 +0100

    fix en las rutas

--- pluggins_git/pluggins_git.sh
+++ pluggins_git/pluggins_git.sh
@@ -3,7 +3,7 @@
 #
 
 DIRECTORIO=$(pwd)
-USUARIO=$(whoami)
+USUARIO=$(sudo -u root echo `whoami`)
 
 function_test() {
 	if [ ! -e /etc ]
@@ -26,7 +26,7 @@ function_git_plugins_color_branch() {
 	wait 
 	sleep 2
 	echo 'Agregando las configuraciones para los branches de GIT'
-	sudo cat $DIRECTORIO/files/git_plugins_color_branch/bashrc >> /home/intranet.biblostravel.com/$USUARIO/.bashrc
+	sudo cat $DIRECTORIO/files/git_plugins_color_branch/bashrc >> /home/marcelo/.bashrc
 	wait 
 	sleep 2
 	echo "Proceso finalizado, reinicie la terminal"
